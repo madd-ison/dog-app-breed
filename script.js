07c7dc7bacaba2f2868b688fa66468062557ba1c
madd-ison <65075658+madd-ison@users.noreply.github.com>
2020-05-29 18:00:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+'use strict';
 
+$(document).ready(function(){
+  console.log("Waiting for submit!")
+  processInput();
+});
+
+
+function findImages(userBreed){
+    fetch('https://dog.ceo/api/breed/' + userBreed + '/images/random')
+    .then(response => response.json())
+    .then(responseJson => 
+    displayImages(responseJson));
+}
+
+function displayImages(responseJson){
+  console.log(responseJson);
+    $('.results-img').replaceWith((`<img src="${responseJson.message}" class="results-img">`))
+    $('.images-set').removeClass('hidden')
+}
+
+
+function processInput(){
+  $('.btn').click(event => {
+    event.preventDefault();
+    findImages()
+    let userBreed = $('#picValue').val();
+    findImages(userBreed);
+  });
+}
